f42210396f63bc0182025744cad3dab4b90aefed
Varun <nirwan.varun@gmail.com>
2021-06-24 15:44:47 +0530

    First initial commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+#Terraform backend configuration block
+terraform {
+  backend "azurerm" {
+      resource_group_name = "githubactionsrg"
+      storage_account_name = "github21actions"
+      container_name = "githubactionscontainer"
+      key = "vnterraform.tfstate"    
+  }
+}
+
+
+/*
+* Resource Group
+*/
+resource "azurerm_resource_group" "azure_rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
