c5cafcff9e3c9eb1ea720eb57d84d2dedc41ea49
VarunNirwan <nirwan.varun@gmail.com>
2021-09-01 19:01:05 +0530

    ssasa

--- 07_Azure_LogicApp.tf
+++ 07_Azure_LogicApp.tf
@@ -13,7 +13,7 @@ resource "azurerm_template_deployment" "terraform-arm" {
     #"workflows_logicapp321_name " = "logicapp321" 
     #"connections_azureblob_1_externalid" = "/subscriptions/28b43add-147d-4e0a-ab1e-43528d4d9205/resourceGroups/testlogicapp/providers/Microsoft.Web/connections/azureblob-1"
     "workflows_tb_sb_logicapp_test_name" = "tb-pg-logicapp"
-  }
+  } 
 
   deployment_mode = "Incremental"
 }
