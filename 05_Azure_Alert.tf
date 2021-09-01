ef9cd1ac902967bcc42d1dd61807f8e5a2dcf195
VarunNirwan <nirwan.varun@gmail.com>
2021-09-01 19:02:01 +0530

    vddses

--- 05_Azure_Alert.tf
+++ 05_Azure_Alert.tf
@@ -11,7 +11,7 @@ resource "azurerm_monitor_action_group" "main" {
   }
 }
 
-# Alert updated   demo
+# Alert updated   demo 
 resource "azurerm_monitor_metric_alert" "serverexception" {
   name                = "ServerException"
   resource_group_name = azurerm_resource_group.azure_rg.name
